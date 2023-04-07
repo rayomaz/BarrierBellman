521b287dbca3f0e4e2ca90a8eb9b2916d4844743
Frederik Mathiesen <frederik_mathies@hotmail.com>
2023-04-07 13:02:37 +0200

    Remove constants as they were introducing silent errors

--- PiecewiseBarrier/src/constants.jl
+++ PiecewiseBarrier/src/constants.jl
@@ -1,5 +1,10 @@
 # Optimization constants
 const ϵ = 1e-6                  # Precision parameter
-const barrier_degree  = 2       # Barrier degree
-const lagrange_degree = 2       # Lagragian multiplier degree
-const σ_noise = 0.1             # Standard deviation
+
+### Commented out as they were silently introducing errors
+### The problem is that if you use different σ_noise, barrier polynomial, or lagrange_degree, 
+### by defining them somewere but using these constants elsewhere then the two methods are not
+### comparable. Instead take them all as parameters and be sure they are well-defined there.
+# const barrier_degree  = 2       # Barrier degree
+# const lagrange_degree = 2       # Lagragian multiplier degree
+# const σ_noise = 0.1             # Standard deviation
