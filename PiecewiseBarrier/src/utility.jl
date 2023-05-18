2e38a3d4e2345094a3db1d261ea98dcbca4c8e2f
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-05-18 22:58:36 +0200

    Add asymmetric_dual_constraint! for a robust LP constraint with polyhedral uncertainty.

--- PiecewiseBarrier/src/utility.jl
+++ PiecewiseBarrier/src/utility.jl
@@ -25,3 +25,21 @@ function state_space_generation(state_partitions)
     return state_space
 
 end
+
+
+function asymmetric_dual_constraint!(model, c, rhs, H, h)
+    # Constraints of the form              [max_x  cᵀx, s.t. Hx ≤ h] ≤ rhs
+    # reformulated to the asymmetric dual  [min_{y ≥ 0} yᵀh, s.t. Hᵀx = c] ≤ rhs
+    # and lifted to the outer minimization.
+    # s.t. y ≥ 0
+    #      yᵀh ≤ rhs
+    #      Hᵀx = c
+
+    m = size(H, 1)
+    y = @variable(model, [1:m], lower_bound = 0)
+
+    H, h = tosimplehrep(normalize(HPolyhedron(H, h)))
+
+    @constraint(model, y ⋅ h ≤ rhs)
+    @constraint(model, H' * y .== c)
+end
