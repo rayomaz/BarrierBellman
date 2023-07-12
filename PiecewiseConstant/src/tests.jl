e7316b09fc2ed6f8f48c534b095e95f11f013ed1
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-07-12 10:21:55 +0200

    remove read_regions from tests

--- PiecewiseConstant/src/tests.jl
+++ PiecewiseConstant/src/tests.jl
@@ -4,35 +4,6 @@
 
 """
 
-export read_regions
-
-function read_regions(partitions::MatlabFile, probabilities::MatlabFile)
-    # Load partition matrices
-    regions = read(partitions, "partitions")
-    regions_lower = regions[:, 1, :]
-    regions_upper = regions[:, 2, :]
-
-    # Load probability matrices
-    prob_lower = read(probabilities, "matrix_prob_lower")
-    prob_upper = read(probabilities, "matrix_prob_upper")
-    prob_unsafe_lower = read(probabilities, "matrix_prob_unsafe_lower")'
-    prob_unsafe_upper = read(probabilities, "matrix_prob_unsafe_upper")'
-
-    regions = [
-        RegionWithProbabilities(
-            Hyperrectangle(low=X̲, high=X̅), (copy(P̲), copy(P̅)), (copy(P̲ᵤ[1]), copy(P̅ᵤ[1]))
-        ) for (X̲, X̅, P̲, P̅, P̲ᵤ, P̅ᵤ) in zip(
-            eachrow(regions_lower),
-            eachrow(regions_upper),
-            eachrow(prob_lower),
-            eachrow(prob_upper),
-            eachrow(prob_unsafe_lower),
-            eachrow(prob_unsafe_upper)
-        )]
-
-    return regions
-end
-
 function sum_probabilities(jj, probabilities::MatlabFile)
     # Bounds
     prob_lower = read(probabilities, "matrix_prob_lower")
