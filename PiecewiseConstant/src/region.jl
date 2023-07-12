1bd528fc17d5efb34dbb3348cb96f42e5d653247
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-07-12 12:23:26 +0200

    Add safety check for transition probabilities

--- PiecewiseConstant/src/region.jl
+++ PiecewiseConstant/src/region.jl
@@ -2,6 +2,18 @@ struct RegionWithProbabilities{T, S<:LazySet{T}}
     region::S
     transition_to_other_regions::Tuple{Vector{T}, Vector{T}}
     transition_to_unsafe::Tuple{T, T}
+
+    function RegionWithProbabilities(region::S, transition_to_other_regions::Tuple{Vector{T}, Vector{T}}, transition_to_unsafe::Tuple{T, T}) where {T, S<:LazySet{T}}
+        # Include custom constructor only for safety checks
+
+        joint_lower_bound = sum(transition_to_other_regions[1]) + transition_to_unsafe[1]
+        @assert joint_lower_bound <= 1 "The joint lower bound transition probability (is $joint_lower_bound) should be less than or equal to 1."
+
+        joint_upper_bound = sum(transition_to_other_regions[2]) + transition_to_unsafe[2]
+        @assert joint_upper_bound >= 1 "The joint upper bound transition probability (is $joint_upper_bound) should be greater than or equal to 1."
+
+        return new{T, S}(region, transition_to_other_regions, transition_to_unsafe)
+    end
 end
 
 region(X::RegionWithProbabilities) = X.region
