e57874b6e3322847fb8dee35cf3d48d9e0762dc5
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-07-13 18:46:31 +0200

    Close files after reading transition probabilities

--- PiecewiseConstant/synthesize/linear.jl
+++ PiecewiseConstant/synthesize/linear.jl
@@ -17,6 +17,7 @@ probabilities = "models/$system_flag/probability_data_$(number_hypercubes)_sigma
 probabilities = matopen(probabilities)
 regions = [Interval(-0.25, -0.15), Interval(-0.15, -0.05), Interval(-0.05, 0.05), Interval(0.05, 0.15), Interval(0.15, 0.25)]
 regions = read_regions(regions, probabilities)
+close(probabilities)
 
 initial_region = Interval(-0.05, 0.05)
 obstacle_region = EmptySet(1)
@@ -24,9 +25,12 @@ obstacle_region = EmptySet(1)
 # Optimize: method 1 (revise beta values)
 @time B, beta = constant_barrier(regions, initial_region, obstacle_region)
 @time beta_updated, p_distribution = post_compute_beta(B, regions)
+println(beta_updated)
 # @btime beta_updated = accelerated_post_compute_beta(B, regions)
 
 # Optimize: method 2 (dual approach)
 @time b_dual, beta_dual = dual_constant_barrier(regions, initial_region, obstacle_region)
+@time beta_dual_updated, p_distribution = post_compute_beta(b_dual, regions)
+println(beta_dual_updated)
 
 println("\n Linear model verified.")
