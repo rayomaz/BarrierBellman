dc89eedeedb3b8abbe00a5756641f5eebebf750d
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-07-19 16:08:09 +0200

    Remove :static task-thread allocation - accelerated_post_compute already uses task_local_storage and :static does not play well with @btime.

--- PiecewiseConstant/synthesize/linear.jl
+++ PiecewiseConstant/synthesize/linear.jl
@@ -31,7 +31,7 @@ obstacle_region = EmptySet(1)
 @time B, beta = constant_barrier(regions, initial_region, obstacle_region)
 @time beta_updated, p_distribution = post_compute_beta(B, regions)
 # println(beta_updated)
-# @btime beta_updated = accelerated_post_compute_beta(B, regions)
+@time beta_updated = accelerated_post_compute_beta(B, regions)
 
 # Optimize: method 2 (dual approach)
 @time B_dual, beta_dual = dual_constant_barrier(regions, initial_region, obstacle_region)
