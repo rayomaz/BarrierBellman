ce72b9ae5e0fa191897da27b3361abe0c24eee04
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-07-20 11:28:09 +0200

    Save as NetCDF for linear

--- PiecewiseConstant/synthesize/probability_linear.jl
+++ PiecewiseConstant/synthesize/probability_linear.jl
@@ -10,35 +10,26 @@ using PiecewiseConstant
 
 using LazySets
 
+using YAXArrays, NetCDF
 using DelimitedFiles
 using MAT
 
 # System
-A = [1.05][:, :]
+f = 1.05
+A = [f][:, :]
 b = [0.0]
 σ = [0.01]
 
 system = AdditiveGaussianLinearSystem(A, b, σ)
 
 # State partitions
-filename = "models/linear/state_partitions.txt"
-state_partitions = readdlm(joinpath(@__DIR__, filename), ' ')
-state_partitions = [Hyperrectangle(low=[low], high=[high]) for (low, high) in eachrow(state_partitions)]
+num_regions = 5
+a = range(-0.25, 0.25, length=num_regions + 1)
+state_partitions = [Hyperrectangle(low=[low], high=[high]) for (low, high) in zip(a[1:end-1], a[2:end])]
 
 # Extract probability data
 @time probability_bounds = transition_probabilities(system, state_partitions)
 
-(matrix_prob_lower, 
- matrix_prob_upper,
- matrix_prob_unsafe_lower,
- matrix_prob_unsafe_upper) = probability_bounds
-
-# Save data
-data = Dict("matrix_prob_lower" => matrix_prob_lower,
-            "matrix_prob_upper" => matrix_prob_upper,
-            "matrix_prob_unsafe_lower" => matrix_prob_unsafe_lower,
-            "matrix_prob_unsafe_upper" => matrix_prob_unsafe_upper)
-
-# Save the dictionary in a .mat file
-filename = "models/linear/probability_data_$(length(state_partitions))_sigma_$σ.mat"
-matwrite(joinpath(@__DIR__, filename), data)
+# Save to a .nc file
+filename = "models/linear/probability_data_$(length(state_partitions))_f_$(f)_sigma_$σ.nc"
+savedataset(probability_bounds; path=joinpath(@__DIR__, filename), driver=:netcdf, overwrite=true)
