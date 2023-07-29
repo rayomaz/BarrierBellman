9a9bc2286abfa05516b3e120163afbf8bdcb0b85
Rayan Mazouz <rayan.mazouz@colorado.edu>
2023-07-29 15:05:18 -0600

    Removing double pieces of code

--- PiecewiseConstant/src/iterative_barrier.jl
+++ PiecewiseConstant/src/iterative_barrier.jl
@@ -1,21 +1,22 @@
-function iterative_barrier(regions, initial_region, obstacle_region; guided = true, δ = 0.05, max_iterations=40)
+function iterative_barrier(regions, initial_region, obstacle_region; guided = true, δ = 0.025, max_iterations=80)
     iteration_prob = regions
     
     B, beta = constant_barrier(iteration_prob, initial_region, obstacle_region)
     beta_updated, p_distribution = accelerated_post_compute_beta(B, regions)
     
+    println("Iterations: ", max_iterations)
+
     for i in 1:(max_iterations - 1)
+
+        iteration_prob = update_regions(iteration_prob, p_distribution)
     
         if guided == false
-            iteration_prob = update_regions(iteration_prob, p_distribution)
             B, beta = constant_barrier(iteration_prob, initial_region, obstacle_region)
-            beta_updated, p_distribution = accelerated_post_compute_beta(B, regions)
         elseif guided == true
-            # println(i, ", ", δ)
-            iteration_prob = update_regions(iteration_prob, p_distribution)
             B, beta = guided_constant_barrier(iteration_prob, initial_region, obstacle_region, B, δ)
-            beta_updated, p_distribution = accelerated_post_compute_beta(B, regions)
         end
+
+        beta_updated, p_distribution = accelerated_post_compute_beta(B, regions)
     end
 
     return B, beta_updated
