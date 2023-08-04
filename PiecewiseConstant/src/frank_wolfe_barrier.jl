0bcc8b1ec88d6f82e0df1633a612bb785034fa5a
Frederik Mathiesen <frederik_mathies@hotmail.com>
2023-08-05 01:11:02 +0200

    Implemented IVI ordering

--- PiecewiseConstant/src/frank_wolfe_barrier.jl
+++ PiecewiseConstant/src/frank_wolfe_barrier.jl
@@ -10,93 +10,77 @@ function frank_wolfe_barrier(regions::Vector{<:RegionWithProbabilities}, initial
 
     # Construct linear minization oracles
     barrier_oracle = BarrierOracle{Float64}()
-    transition_prob_oracle = FrankWolfe.ProbabilitySimplexOracle{Float64}()
-    # TODO: Add transition bound oracle
-
-    direction_indices = [[1:n]; [(n+(i-1)*(n+1)+1):(n+i*(n+1)) for i in 1:n]]
-    lmo = ProductOracle([[barrier_oracle]; [IntervalProbabilityOracle([prob_lower(regions[i]); [prob_unsafe_lower(regions[i])]], [prob_upper(regions[i]); [prob_unsafe_upper(regions[i])]]) for i in 1:n]...], direction_indices)
 
     # Set-up loss function and gradient
     η_indices = findall(X -> !isdisjoint(initial_region, region(X)), regions)
+    cache = zeros(n + 1)
 
-    function loss(x)
-        b = x[1:n]
-        p = reshape(x[n+1:end], n + 1, n)
-        p, pᵤ = p[1:end-1, :], p[end, :]
+    function loss(b)
+        b_ext = [b; [1]]
+        q = sortperm(b_ext, rev=true)
 
-        exp = vec(reshape(b, 1, :) * p)
-
-        βⱼ = @. max(exp + pᵤ - b, 0)
-
-        β = maximum(βⱼ)
+        β = maximum(1:n) do j
+            p = ivi_prob!(cache, regions[j], q)
+            exp = dot(p, b_ext)
+    
+            βⱼ = max(exp - b[j], 0)
+            return βⱼ
+        end
         η = maximum(b[η_indices])
 
         return η + β * time_horizon
     end
 
-    function grad!(storage, x)
-        g = gradient(loss, x)[1]
+    function grad!(storage, b)
+        b_ext = [b; [1]]
+        q = sortperm(b_ext, rev=true)
 
-        storage[direction_indices[1]] = g[direction_indices[1]]
-
-        for idx in direction_indices[2:end]
-            storage[idx] .= -g[idx]
+        βⱼ, j = findmax(1:n) do j
+            p = ivi_prob!(cache, regions[j], q)
+            exp = dot(p, b_ext)
+    
+            return exp - b[j]
         end
 
-        println(storage)
+        if βⱼ > 0
+            pⱼ = ivi_prob(regions[j], q)
+            storage .= pⱼ[1:end - 1]
+            storage[j] += -1
+        else
+            fill!(storage, 0)
+        end
+
+        j = η_indices[argmax(b[η_indices])]
+        storage[j] += 1
 
         return storage
     end
 
     # Initial point
-    d₀ = collect(-ones(n + (n + 1) * n))
-    x₀ = FrankWolfe.compute_extreme_point(lmo, d₀)
-    println(x₀)
-
-    # Gradient (for structure)
-    ∇x₀ = collect(ones(n + (n + 1) * n))
-    grad!(∇x₀, x₀)
+    d₀ = collect(-ones(n))
+    x₀ = FrankWolfe.compute_extreme_point(barrier_oracle, d₀)
 
     x_lmo, v, primal, dual_gap, trajectory_lmo = FrankWolfe.frank_wolfe(
-        loss, grad!, lmo, x₀;
-        max_iteration=100,
+        loss, grad!, barrier_oracle, x₀;
+        max_iteration=10000,
+        momentum=0.8,
         line_search=FrankWolfe.Agnostic(),
-        print_iter=100 / 10,
-        memory_mode=FrankWolfe.OutplaceEmphasis(),
+        print_iter=100,
+        memory_mode=FrankWolfe.InplaceEmphasis(),
         verbose=true
     )
 
-    B = x_lmo[direction_indices[1]]
+    B = x_lmo
     η = maximum(B[η_indices])
 
-    p1 = reshape(x_lmo[n + 1:end], n + 1, n)
-    println(map(sum, eachcol(p1)))
-    p1, pᵤ1 = p1[1:end-1, :], p1[end, :]
-    println(B)
-    println(prob_unsafe_lower.(regions))
-    println(prob_unsafe_upper.(regions))
-    println(pᵤ1)
+    
+    b_ext = [B; [1]]
+    q = sortperm(b_ext, rev=true)
 
-    exp1 = vec(reshape(B, 1, :) * p1)
-    β1 = max(dot(B, p1[:, 1]) + pᵤ1[1] - B[1], 0)
-    println(β1)
+    p = ivi_prob.(regions, tuple(q))
+    exp = dot.(p, tuple(b_ext))
 
-    println(prob_lower(regions[1]) .<= p1[:, 1])
-    println(prob_upper(regions[1]) .>= p1[:, 1])
-    println(prob_unsafe_lower(regions[1]) <= pᵤ1[1])
-    println(prob_unsafe_upper(regions[1]) >= pᵤ1[1])
-
-    βⱼ = @. max(exp1 + pᵤ1 - B, 0)
-    println(βⱼ)
-
-    println(grad!(∇x₀, x_lmo))
-    # βⱼ = map(enumerate(direction_indices[2:end])) do (j, idx)
-    #     pⱼ = x_lmo[idx]
-    #     p = @view pⱼ[1:end-1]
-    #     pᵤ = pⱼ[end]
-
-    #     return max(dot(B, p) + pᵤ - B[j], 0)
-    # end
+    βⱼ = @. max(exp - B, 0)
 
     @info "Solution Frank-Wolfe" η β = maximum(βⱼ)
 
@@ -106,60 +90,13 @@ end
 struct BarrierOracle{T} <: FrankWolfe.LinearMinimizationOracle end
 
 function FrankWolfe.compute_extreme_point(lmo::BarrierOracle, direction;
+    v=similar(direction),
     kwargs...)
 
-    v = zero(direction)
-    for i in eachindex(direction)
-        if direction[i] < 0
+    fill!(v, 0)
+    @inbounds for (i, d) in enumerate(direction)
+        if d < 0
             v[i] = 1
-        end
-    end
-    return v
-end
-
-struct ProductOracle <: FrankWolfe.LinearMinimizationOracle
-    lmos::Vector{FrankWolfe.LinearMinimizationOracle}
-    indices::Vector{AbstractArray}
-end
-
-function FrankWolfe.compute_extreme_point(
-    lmo::ProductOracle,
-    direction::AbstractArray;
-    storage=similar(direction),
-    kwargs...
-)
-    for (l, indices) in zip(lmo.lmos, lmo.indices)
-        storage[indices] .= compute_extreme_point(l, direction[indices]; kwargs...)
-    end
-    return storage
-end
-
-struct IntervalProbabilityOracle{T,VT<:AbstractVector{T}} <: FrankWolfe.LinearMinimizationOracle
-    lower_bounds::VT
-    upper_bounds::VT
-
-    total_lower::T
-end
-IntervalProbabilityOracle(lower_bound, upper_bound) = IntervalProbabilityOracle(lower_bound, upper_bound, sum(lower_bound))
-
-function FrankWolfe.compute_extreme_point(
-    lmo::IntervalProbabilityOracle{T},
-    direction;
-    kwargs...
-) where {T}
-
-    v = copy(lmo.lower_bounds)
-    remaining = T(1) - lmo.total_lower
-
-    p = sortperm(direction)
-    for i in p
-        # println((sum(v), remaining, lmo.lower_bounds[i], lmo.upper_bounds[i]))
-        if lmo.upper_bounds[i] < lmo.lower_bounds[i] + remaining
-            v[i] = lmo.upper_bounds[i]
-            remaining = max(T(0), remaining + lmo.lower_bounds[i] - lmo.upper_bounds[i])
-        else
-            v[i] += remaining
-            break
         end
     end
     return v
--- PiecewiseConstant/src/region.jl
+++ PiecewiseConstant/src/region.jl
@@ -1,7 +1,11 @@
 struct RegionWithProbabilities{T, S<:LazySet{T}}
     region::S
-    transition_to_other_regions::Tuple{Vector{T}, Vector{T}}
-    transition_to_unsafe::Tuple{T, T}
+    lower::Vector{T}
+    upper::Vector{T}
+    gap::Vector{T}
+
+    sum_lower::T
+    sum_upper::T
 
     function RegionWithProbabilities(region::S, transition_to_other_regions::Tuple{Vector{T}, Vector{T}}, transition_to_unsafe::Tuple{T, T}) where {T, S<:LazySet{T}}
         # Include custom constructor only for safety checks
@@ -12,15 +16,40 @@ struct RegionWithProbabilities{T, S<:LazySet{T}}
         joint_upper_bound = sum(transition_to_other_regions[2]) + transition_to_unsafe[2]
         @assert joint_upper_bound >= 1 - 10 * eps(Float64) "The joint upper bound transition probability (is $joint_upper_bound) should be greater than or equal to 1."
 
-        return new{T, S}(region, transition_to_other_regions, transition_to_unsafe)
+        lower, upper = vcat(transition_to_other_regions[1], transition_to_unsafe[1]),
+                       vcat(transition_to_other_regions[2], transition_to_unsafe[2])
+
+        return new{T, S}(region, lower, upper, upper - lower, sum(lower), sum(upper))
     end
 end
 
 region(X::RegionWithProbabilities) = X.region
-prob_lower(X::RegionWithProbabilities) = X.transition_to_other_regions[1]
-prob_upper(X::RegionWithProbabilities) = X.transition_to_other_regions[2]
-prob_unsafe_lower(X::RegionWithProbabilities) = X.transition_to_unsafe[1]
-prob_unsafe_upper(X::RegionWithProbabilities) = X.transition_to_unsafe[2]
+prob_lower(X::RegionWithProbabilities) = X.lower[1:end - 1]
+prob_upper(X::RegionWithProbabilities) = X.upper[1:end - 1]
+prob_unsafe_lower(X::RegionWithProbabilities) = X.lower[end]
+prob_unsafe_upper(X::RegionWithProbabilities) = X.upper[end]
+
+function ivi_prob(X::RegionWithProbabilities, p::AbstractVector{<:Int})
+    v = Vector{Float64}(undef, length(p))
+    return ivi_prob!(v, X, p)
+end
+
+function ivi_prob!(cache, X::RegionWithProbabilities, p::AbstractVector{<:Int})
+    copyto!(cache, X.lower)
+    
+    remaining = 1 - X.sum_lower
+    
+    @inbounds for i in p
+        if X.gap[i] < remaining
+            remaining -= X.gap[i]
+            cache[i] = X.upper[i]
+        else
+            cache[i] += remaining
+            break
+        end
+    end
+    return cache
+end
 
 function update_regions(regions::Vector{<:RegionWithProbabilities}, p_distribution::Matrix{Float64})
     new_regions = Vector{RegionWithProbabilities}(undef, length(regions))
