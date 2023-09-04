84ab9cd8c8f776c941a94d6d14d36c15dc9b8168
Rayan Mazouz <rayan.mazouz@colorado.edu>
2023-09-04 15:53:17 -0600

    Adding 4 methods for PWC for single_mode

--- StochasticBarrierFunctions/synthesize/single_mode.jl
+++ StochasticBarrierFunctions/synthesize/single_mode.jl
@@ -1,4 +1,4 @@
-""" Piecewise Barrier Function: Neural Network Dynamic Model [Pendulum]
+""" Piecewise Barrier Function: Linear Dynamic Model [Single Mode]
 
     © Rayan Mazouz
 
@@ -11,29 +11,17 @@ using YAXArrays, NetCDF
 
 # System
 F = 1.0
-dim = 1
+dim = 2
 
 A = F*I(dim)
 b = zeros(dim)
 σ = 0.1*ones(dim)
 
-system = AdditiveGaussianLinearSystem(A, b, σ)
+num_regions = 64
+filename = "models/single_mode/probability_data_$(num_regions)_f_$(f)_sigma_$σ.nc"
+dataset = open_dataset(joinpath(@__DIR__, filename))
 
-# Create partitions
-Δ = 0.04
-lower_bound = -1.0
-upper_bound = 0.5
-Q = Int(round((upper_bound - lower_bound) / Δ))
-x = range(lower_bound, upper_bound, length=Q+1)
-
-if dim == 1
-    state_partitions = [Hyperrectangle(low=[low_x], high=[high_x]) 
-                        for (low_x, high_x) in zip(x[1:end-1], x[2:end])]
-elseif dim == 2
-    state_partitions = [Hyperrectangle(low=[low_x, low_y], high=[high_x, high_y]) 
-                        for (low_x, high_x) in zip(x[1:end-1], x[2:end])
-                            for (low_y, high_y) in zip(x[1:end-1], x[2:end])]
-end
+probabilities = load_probabilities(dataset)
 
 # Initial range and obstacle space
 initial_range, obstacle_range = 0.25, 0.02
@@ -42,5 +30,19 @@ initial_region = Hyperrectangle([-0.70, -0.1], initial_range*ones(dim))
 
 obstacle1 = Hyperrectangle([-0.55, 0.30], obstacle_range*ones(dim))
 obstacle2 = Hyperrectangle([-0.55, -0.15], obstacle_range*ones(dim))
-obstacle_region = obstacle1, obstacle2
+obstacle_region = union(obstacle1, obstacle2)
 # obstacle_region = EmptySet(dim)
+
+# Optimize: method 1 (revise beta values)
+@time B_ub, beta_ub = synthesize_barrier(UpperBoundAlgorithm(), probabilities, initial_region, obstacle_region)
+
+# Optimize: method 2 (dual approach)
+@time B_dual, beta_dual = synthesize_barrier(DualAlgorithm(), probabilities, initial_region, obstacle_region)
+
+# Optimize: method 3 (iterative approach)
+@time B_it, beta_it = synthesize_barrier(IterativeUpperBoundAlgorithm(), probabilities, initial_region, obstacle_region)
+
+# Optimize: method 4 (project gradient descent approach)
+@time B_pgd, beta_pgd = synthesize_barrier(GradientDescentAlgorithm(), probabilities, initial_region, obstacle_region)
+
+println("Single mode grid verified.")
