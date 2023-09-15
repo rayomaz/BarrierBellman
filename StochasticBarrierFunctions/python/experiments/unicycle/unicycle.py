dae71bfb9cb6a9ecb79c485808f8e14fd2f1678f
Rayan Mazouz <rayan.mazouz@colorado.edu>
2023-09-14 18:40:10 -0600

    Adding unicycle.py

---
+++ StochasticBarrierFunctions/python/experiments/unicycle/unicycle.py
@@ -0,0 +1,23 @@
+import logging
+
+from bounds.bounds import BarrierBoundModelFactory
+
+from .dynamics import Unicycle
+from experiments.utils import grid_partition
+
+logger = logging.getLogger(__name__)
+
+
+class UnicycleExperiment:
+
+    def __init__(self, args, config):
+        logger.info('Constructing model')
+
+        self.factory = BarrierBoundModelFactory()
+        self.dynamics = Unicycle(config['dynamics']).to(args.device)
+
+        self.config = config
+        self.args = args
+
+    def grid_partition(self):
+        return grid_partition(self.args, self.config)
