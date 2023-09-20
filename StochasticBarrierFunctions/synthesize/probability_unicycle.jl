6e5930e7181426c6baf2a954662029a5f2229ee2
Rayan Mazouz <rayan.mazouz@colorado.edu>
2023-09-19 21:00:11 -0600

    Update probability_unicycle.jl

--- StochasticBarrierFunctions/synthesize/probability_unicycle.jl
+++ StochasticBarrierFunctions/synthesize/probability_unicycle.jl
@@ -5,8 +5,8 @@ using YAXArrays, NetCDF
 
 # System
 system_flag = "unicycle"
-number_hypercubes = 4096
-σ = [0.1, 0.1, 0.01, 0.1]
+number_hypercubes = 12000
+σ = [0.1, 0.1, 0.05, 0.1]
 
 filename = "../data/$system_flag/dynamics_$number_hypercubes.nc"
 dataset = open_dataset(joinpath(@__DIR__, filename))
