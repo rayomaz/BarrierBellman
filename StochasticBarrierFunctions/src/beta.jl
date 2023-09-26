9177427b69f79cdb4d09dd7895eb4cea59288338
Rayan Mazouz <rayan.mazouz@colorado.edu>
2023-09-26 16:39:34 -0600

    Adding O-maxization approach for update

--- StochasticBarrierFunctions/src/beta.jl
+++ StochasticBarrierFunctions/src/beta.jl
@@ -61,3 +61,55 @@ function verify_beta(B, regions::Vector{<:RegionWithProbabilities{T}}) where {T}
 
     return β_parts, p_distribution
 end
+
+function OMaximization(B, indBSorted, indBSorted_perm, regions::Vector{<:RegionWithProbabilities{T}}) where {T}
+
+    # O Maximization approach for quick sorting
+    # Based on Section V.A of https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=7029024
+
+    β_parts = Vector{T}(undef, length(indBSorted))
+    p_distribution = [zero(region.gap) for region in regions]
+
+    Threads.@threads for jj in eachindex(regions)
+        Xⱼ, Bⱼ = regions[jj], B[jj]
+
+        P̲, P̅ = prob_lower(Xⱼ), prob_upper(Xⱼ)
+
+        P̲ᵤ, P̅ᵤ = prob_unsafe_lower(Xⱼ), prob_unsafe_upper(Xⱼ)
+
+        # Append P̲ and P̲ᵤ
+        P̲ = [P̲; P̲ᵤ]
+
+        # Append P̅ and P̅ᵤ
+        P̅ = [P̅; P̅ᵤ]
+
+        # O-maximization
+        used = sum(P̲)
+        remain = 1 - used
+
+        # Initialize p_maximization
+        p_maximization = zeros(1,length(indBSorted ))
+
+        for kk in eachindex(indBSorted)
+
+            actual_index = indBSorted_perm[kk]
+
+            if P̅[actual_index] <= (remain + P̲[actual_index])
+                p_maximization[actual_index] = P̅[actual_index]
+            else
+                p_maximization[actual_index] = P̲[actual_index] + remain
+            end
+
+            # Update remain
+            remain = maximum([0, remain - (P̅[actual_index] - P̲[actual_index]) ])
+
+        end
+
+        @inbounds β_parts[jj] = dot(p_maximization, [B[1:5]; 1.0]) - Bⱼ
+        @inbounds copyto!(p_distribution[jj], p_maximization)
+
+    end
+
+    return β_parts, p_distribution
+    
+end
