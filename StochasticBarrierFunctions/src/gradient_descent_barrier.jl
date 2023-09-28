0fbfc7f5b54a23ca93bbd2e1d15949d53a39c352
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-09-28 13:16:19 +0200

    Add progressbar to GD

--- StochasticBarrierFunctions/src/gradient_descent_barrier.jl
+++ StochasticBarrierFunctions/src/gradient_descent_barrier.jl
@@ -13,7 +13,8 @@ function synthesize_barrier(alg::GradientDescentAlgorithm, regions::Vector{<:Reg
 
     state = Optimisers.setup(optim, ws.B)
 
-        state = gradient_descent_barrier_iteration!(ws, state, regions, p, q, decay(k))
+    @showprogress for k in 0:alg.num_iterations
+        state = gradient_descent_barrier_iteration!(ws, state, regions, p, q, decay(k); time_horizon=time_horizon)
     end
 
     η = maximum(ws.B_init)
