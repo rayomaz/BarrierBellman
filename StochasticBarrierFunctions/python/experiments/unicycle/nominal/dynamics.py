34f01b82abe33fdacfc47cb4bf235819018f882a
Rayan Mazouz <rayan.mazouz@colorado.edu>
2023-10-05 09:52:49 -0600

    Adjusting dynamics.py to PD control for unicycle nominal

--- StochasticBarrierFunctions/python/experiments/unicycle/nominal/dynamics.py
+++ StochasticBarrierFunctions/python/experiments/unicycle/nominal/dynamics.py
@@ -5,68 +5,87 @@ from bound_propagation import FixedLinear, Parallel, Select, Cos, Sin, Cat, Mul,
 
 from bounds.dynamics import AdditiveGaussianDynamics
 
-
-class UnicycleController(FixedLinear):
-    def __init__(self):
-        # Gain using optimal LQR controller
-        F = torch.as_tensor([
-            [0.0977882,   9.77833e-6,  -9.956e-11,  0.442348],
-            [9.77783e-7,  9.98119e-6,   1.0e-5,     4.42348e-6]
-        ])
-
-        # Negative feedback control
-        super().__init__(-F)
-
-
 class Unicycle(nn.Sequential, AdditiveGaussianDynamics):
     def __init__(self, dynamics_config):
         
+        # Load control parameters
         Ts = dynamics_config['Ts']
+        kp1 = dynamics_config['kp1']
+        kd1 = dynamics_config['kd1']
+        kp2 = dynamics_config['kp2']
+        kd2 = dynamics_config['kd2']
 
         # Choose ODE formulation z = (x, y, θ, v)
         # ẋ = z₄ cos z₃
         # ẏ = z₄ sin z₃
         # θ̇ = - [(sin z₃) / z₄ ] * u₁ + [(cos z₃) / z₄] * u₂
         # v̇ = cos z₃ * u₁ + sin z₃ * u₂
-
+        
+        # Nominal Control Law for v > 5e-2 
+        # u₁ = - kp1 * x - kd1 * v * cos theta
+        # u₂ = - kp2 * y - kd2 * v * sin theta
+        
         # Discretize with Euler x(k + 1) = x(k) + Ts * dx(k)
 
         super().__init__(
             Parallel(
-                nn.Identity(),  # (x, y, theta, v)
+                nn.Identity(),  # (x, y, theta, v) #(0, 1, 2, 3)
                 nn.Sequential(
                     Select([2]),  # theta
                     Parallel(
                         Sin(),
                         Cos()
-                    )  # (sin theta, cos theta)
+                    )  # (sin theta, cos theta) #(4, 5)
                 ),
-                UnicycleController()  # (u1, u2)
             ),
             Cat(
                 Mul(
-                    Select([3, 3, 6, 6, 7, 7]),
-                    Select([4, 5, 4, 5, 4, 5])
-                )  # (v * sin theta, v * cos theta, u1 * sin theta, u1 * cos theta, u2 * sin theta, u2 * cos theta)
+                    Select([3, 3]),
+                    Select([4, 5])
+                )  # (v * sin theta, v * cos theta) #(6, 7)
             ),
             Cat(
                 Div(
-                    Select([10, 13]),
+                    Select([4, 5]),
                     Select([3, 3])
-                )  # (u1 * sin theta / v, u2 * cos theta / v)
+                )  # (sin theta / v, cos theta / v) #(8, 9)
+            ),
+            Cat(
+                Mul(
+                    Select([0, 1]),
+                    Select([5, 4])
+                )  # (x * cos theta, y * sin theta) #(10, 11)
+            ),
+            Cat(
+                Mul(
+                    Select([0, 1]),
+                    Select([8, 9])
+                )  # (x * sin theta / v, y * cos theta / v) #(12, 13)
+            ),
+            Cat(
+                Mul(
+                    Select([7, 6]),
+                    Select([8, 9])
+                )  # (v cos theta * sin theta / v, v sin theta * cos theta / v) #(14, 15)
+            ),
+            Cat(
+                Mul(
+                    Select([7, 6]),
+                    Select([5, 4])
+                )  # (v cos theta * cos theta, v sin theta * sin theta) #(16, 17)
             ),
             FixedLinear(
                 torch.as_tensor([
-                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
                 ]) +
                 Ts * torch.as_tensor([
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0]
+                    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, kp1, -kp2, kd1, -kd2, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -kp1, -kp2, 0, 0, 0, 0, -kd1, -kd2]
                 ])
             )
         )
