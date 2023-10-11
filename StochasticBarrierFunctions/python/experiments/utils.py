7742613cb354dfa433e43392bff8f54f73567a02
Rayan Mazouz <rayan.mazouz@colorado.edu>
2023-10-10 18:19:52 -0600

    Updated utils.py to handle multi-controller input

--- StochasticBarrierFunctions/python/experiments/utils.py
+++ StochasticBarrierFunctions/python/experiments/utils.py
@@ -9,24 +9,35 @@ def load_config(config_path):
 
 
 def grid_partition(args, config):
-    x_lower = config['dynamics']['safe_set'][0]
-    x_upper = config['dynamics']['safe_set'][1]
 
-    cell_widths = []
-    cell_centers = []
+    partitions = []
+    
+    # Number of dynamics laws
+    num_controllers = config['dynamics']['num_controllers']
 
-    for i in range(config['dynamics']['dim']):
-        x_space = torch.linspace(x_lower[i], x_upper[i], config['partitioning']['num_slices'][i] + 1)
+    for j in range(0, num_controllers):
 
-        cell_width = (x_space[1:] - x_space[:-1]) / 2
-        cell_center = (x_space[:-1] + x_space[1:]) / 2
+        x_lower = config['dynamics']['safe_set'][j][0]
+        x_upper = config['dynamics']['safe_set'][j][1]
 
-        cell_widths.append(cell_width)
-        cell_centers.append(cell_center)
+        cell_widths = []
+        cell_centers = []
 
-    width, center = torch.cartesian_prod(*cell_widths), torch.cartesian_prod(*cell_centers)
-    lower_x, upper_x = center - width, center + width
+        for i in range(config['dynamics']['dim']):
+            x_space = torch.linspace(x_lower[i], x_upper[i], config['partitioning']['num_slices'][i] + 1)
 
-    partition = Partition((lower_x, upper_x))
+            cell_width = (x_space[1:] - x_space[:-1]) / 2
+            cell_center = (x_space[:-1] + x_space[1:]) / 2
 
-    return partition
+            cell_widths.append(cell_width)
+            cell_centers.append(cell_center)
+
+        width, center = torch.cartesian_prod(*cell_widths), torch.cartesian_prod(*cell_centers)
+        lower_x, upper_x = center - width, center + width
+
+        partition = Partition((lower_x, upper_x))
+
+        partitions.append(partition)
+
+    return partitions
+
