9e48a3ea383b39351116f82ea0ccd09cffa38402
Rayan Mazouz <rayan.mazouz@colorado.edu>
2024-11-15 15:58:24 -0700

    Debug .bash script

--- StochasticBarrierFunctions/run.bash
+++ StochasticBarrierFunctions/run.bash
@@ -5,6 +5,6 @@ set -e
 case "$1" in
     contraction) 
         echo "Running $1"
-        julia --project=/StochasticBarrierFunctions -e 'yaml_file="benchmarks/linear/systems/contraction/sos.yaml"; include("barrier_synthesis.jl")'
+        julia --project=/StochasticBarrierFunctions -e 'yaml_file="benchmarks/linear/systems/contraction/sos.yaml"; include("benchmarks/barrier_synthesis.jl")'
         ;;
 esac
